bc342cec88b2081498fa25cfc549a0761b601fb6
Daria <shirai.ryu.hanzo.hasashi@gmail.com>
2025-08-10 18:29:22 +0300

    update

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
 document.querySelectorAll('.nav-btn').forEach(button => {
 	button.addEventListener('click', () => {
-		const size = button.textContent.trim(); // приклад: "300x250"
+		const size = button.textContent.trim();
 		const [width, height] = size.split('x');
 		const iframe = document.getElementById('banner-frame');
 		iframe.src = `./banner${width}x${height}.html`;
 		iframe.width = width;
 		iframe.height = height;
 	});
+});
+const buttonList = document.getElementById("button-list");
+
+buttonList.addEventListener("click", (event) => {
+
+	if (event.target.tagName === "BUTTON") {
+
+		document.querySelectorAll("#button-list button")
+			.forEach(btn => btn.classList.remove("active"));
+
+		event.target.classList.add("active");
+	}
 });
